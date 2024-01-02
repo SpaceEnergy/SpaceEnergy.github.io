70526dd2c1a3c1fa2a942dfc49e1459c33cf4400
SpaceEnergy <KiwiEnergy0@gmail.com>
2024-01-02 15:51:15 +0100

    Update index.js

--- projects/counter/index.js
+++ projects/counter/index.js
@@ -1,6 +1,10 @@
 let count = 0;
 const display = document.getElementById("display");
 
+document.getElementById("addOne").addEventListener("click", function() {
+    count ++;
+    display.value = count;
+})
 document.getElementById("removeOne").addEventListener("click", function() {
     count --;
     display.value = count;
@@ -8,8 +12,4 @@ document.getElementById("removeOne").addEventListener("click", function() {
 document.getElementById("reset").addEventListener("click", function() {
     count = 0;
     display.value = this.ariaPlaceholder;
-})
-document.getElementById("addOne").addEventListener("click", function() {
-    count ++;
-    display.value = count;
 })
