7d537835c927c86a5deb87cc7692b8d4ff926f82
SpaceEnergy <KiwiEnergy0@gmail.com>
2024-01-02 14:13:18 +0100

    add: +1, +10, +100 buttons to Counter

--- projects/counter/index.js
+++ projects/counter/index.js
@@ -1,6 +1,14 @@
 let number = 0;
 
 document.getElementById("addOne").onclick = function() {
-    number++;
+    number += 1;
+    document.getElementById("display").value = number;
+}
+document.getElementById("addTen").onclick = function() {
+    number += 10;
+    document.getElementById("display").value = number;
+}
+document.getElementById("addHundred").onclick = function() {
+    number += 100;
     document.getElementById("display").value = number;
 }
