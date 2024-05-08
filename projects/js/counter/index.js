9a2ea91d96cabc0106f3c6ce273eb3a2fa434ef5
SpaceEnergy <KiwiEnergy0@gmail.com>
2024-05-08 22:06:47 +0200

    add: Random number JS project

---
+++ projects/js/counter/index.js
@@ -0,0 +1,15 @@
+let count = 0;
+const display = document.getElementById("display");
+
+document.getElementById("addOne").addEventListener("click", function() {
+    count ++;
+    display.value = count;
+})
+document.getElementById("removeOne").addEventListener("click", function() {
+    count --;
+    display.value = count;
+})
+document.getElementById("reset").addEventListener("click", function() {
+    count = 0;
+    display.value = this.ariaPlaceholder;
+})
---
+++ projects/js/randomNumber/index.js
@@ -0,0 +1,3 @@
+document.getElementById("random").addEventListener("click", function() {
+    display.value = Math.floor(Math.random() * (0 + 200));
+});
