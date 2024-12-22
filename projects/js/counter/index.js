f7fa247b62071bd01dc9829f3dc193477b00ff9e
SpaceEnergy <KiwiEnergy0@gmail.com>
2024-12-22 22:02:56 +0100

    fix: counter JS project

--- projects/js/counter/index.js
+++ projects/js/counter/index.js
@@ -4,10 +4,16 @@ const display = document.getElementById("display");
 document.getElementById("addOne").addEventListener("click", function() {
     count ++;
     display.value = count;
+    if (count === 0) {
+        display.value = this.ariaPlaceholder;
+    }
 })
 document.getElementById("removeOne").addEventListener("click", function() {
     count --;
     display.value = count;
+    if (count === 0) {
+        display.value = this.ariaPlaceholder;
+    }
 })
 document.getElementById("reset").addEventListener("click", function() {
     count = 0;
