e2f7b9c1b6c251df3f7c084b5381cb7f512fcee5
SpaceEnergy <KiwiEnergy0@gmail.com>
2024-12-22 22:54:27 +0100

    feat: custom random number

--- projects/js/randomNumber/index.js
+++ projects/js/randomNumber/index.js
@@ -1,6 +1,8 @@
-const min = 0;
-const max = 200;
+const display = document.getElementById("display");
 
 document.getElementById("random").addEventListener("click", function() {
-    display.value = Math.floor(Math.random() * (max - min));
+    const min = Math.ceil(document.getElementById("min").value);
+    const max = Math.floor(document.getElementById("max").value);
+    
+    display.value = Math.floor(Math.random() * (max - min + 1)) + min;
 });
